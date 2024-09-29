7ca08cc3fa4ac8c59718286041c152e8f8bf7329
Luka Martin <ldm3334@gmail.com>
2024-09-30 09:38:06 +1000

    Remove redundant code

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,17 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { GlobalDataService } from '../global-data/global-data.service';
-//import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskService {
   constructor(private readonly globalDataService: GlobalDataService) {}
-
-  // @Cron(CronExpression.EVERY_6_HOURS)
-  // async updateGlobalData() {
-  //   try {
-  //     await this.globalDataService.getGlobalData();
-  //   } catch (error) {
-  //     console.log('Error updating global data', error);
-  //   }
-  // }
 }
